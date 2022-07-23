69866b734af4010c7bc07692537308150cd8801b
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-07-23 11:24:54 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+    const linkStyles = {
+        display: "inline-block",
+        width: "200px",
+        padding: "12px",
+        margin: "0 12px 12px",
+        background: "purple",
+        textDecoration: "none",
+        color: "white",
+      };
+
+  return (
+    <div className="navbar">
+    <NavLink to="/" style={linkStyles} >
+         Home
+     </NavLink> 
+     <NavLink to="/movies" style={linkStyles}>
+         Movies
+     </NavLink> 
+     <NavLink to="/directors" style={linkStyles}>
+         Directors
+     </NavLink> 
+     <NavLink to="/actors" style={linkStyles}>
+         Actors
+     </NavLink> 
+    </div>
+   );
 }
 
 export default NavBar;
